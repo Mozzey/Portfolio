76dc26f02627dbc0877c39f8851d0a29202dde75
Michael Cacciano <MBCacciano@gmail.com>
2017-06-13 14:23:05 -0400

    work/social media done..html/css done for now..jquery soon

--- JS/functions.js
+++ JS/functions.js
@@ -1,5 +1,16 @@
 $( document ).ready(function() {
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
 
-
+		if (scroll > 420) {
+			$('.nav-bar').animate(
+                        {
+                            'margin-left':'100px'
+                            // to move it towards the right and, probably, off-screen.
+                        }, 1000;
+		} else {
+			
+		}
+	})
 
 });
